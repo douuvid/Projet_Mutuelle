adb1e904810caca36fb4ad5a78696436a3132793
Douuvid <silasiharis@gmail.com>
2023-06-10 19:17:17 +0200

    ref au push precdent

--- Mutuelle.py
+++ Mutuelle.py
@@ -1,12 +1,9 @@
 from PyPDF2 import PdfReader
-import re
-from skllm.config import SKLLMConfig
-from skllm import ZeroShotGPTClassifier
+import csv
+import os
 
 
-
-
-class Files():
+class Files:
     def __init__(self, filename):
         self.filename = filename
         self.parts = []
@@ -15,87 +12,51 @@ class Files():
             reader = PdfReader(filename)
             for page_number, page in enumerate(reader.pages):
                 extracted_text = page.extract_text()
-                print(extracted_text)
-                cleaned_text = self.remove_header_and_footer(extracted_text)
-                
-                # print(f"Page {page_number + 1}:")
-                # print(cleaned_text)
-                # print("--------------------")
-                self.parts.append(cleaned_text)
+                self.parts.append(extracted_text)
         else:
             print("Le fichier n'a pas l'extension .pdf")
 
-    def remove_header_and_footer(self, page_content):
-        lines = page_content.split("\n")
-
-        # Supprimer les lignes d'en-tête (par exemple, les 10 premières lignes)
-        header_lines = lines[:10]
-        clean_lines = lines[10:]
-
-        # Supprimer les lignes de pied de page (par exemple, les 10 dernières lignes)
-        footer_lines = clean_lines[-5:]
-        clean_lines = clean_lines[:-5]
-
-        # Retirer les phrases indésirables
-        # undesired_phrases = ["92076 PARIS LA DEFENSE CEDEX", "N° d’Étude tarifaire : 1237439A l’Attention de",
-        #                      "Etude tarifaire – Entreprise","S.A. au capital de 643 054 425 euros","JADIS ET ASSOCIES SAS","N° Siret : 92187904500018"]
-        # clean_lines = [line for line in clean_lines if not any(phrase in line for phrase in undesired_phrases)]
-
-        # Utiliser une expression régulière pour supprimer la partie spécifique du footer
-        # regex_pattern = r"\d{5}\sPARIS\sLA\sDEFENSE\sCEDEX"
-        # clean_lines = [re.sub(regex_pattern, "", line) for line in clean_lines]
-
-        # Reconstituer le contenu de la page sans les en-têtes, les pieds de page et les phrases indésirables
-        cleaned_content = "\n".join(clean_lines)
-    
-
-        return cleaned_content
-    
-    
     def visitor_body(self, text, cm, tm, fontDict, fontSize):
         y = tm[5]
-        if y > 50 and y < 720:
+        if 50 < y < 720:
             self.parts.append(text)
-            
-    #log a chaque boucle 
+
+    def exporte_csv(self, file):
+        with open(file, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(['text', "label"])
+            for element in self.parts:
+                element_split = element.split("\n")
+                for ligne in element_split:
+                    writer.writerow([ligne, "Null"])
+
+
+dossier = "./Contrat/"
+filename_a = "etude_tarifaire_sante_logo_(1).pdf"
+filename_b = "etude_tarifaire_sante_logo_(2).pdf"
+filename_c = "etude_tarifaire_sante_logo_(3).pdf"
+filename_d = "etude_tarifaire_sante_logo_(4).pdf"
+filename_e = "etude_tarifaire_sante_logo_(5).pdf"
+filename_f = "etude_tarifaire_sante_logo_(6).pdf"
+filename_g = "etude_tarifaire_sante_logo_(7).pdf"
+
+file = {
+    "fichier_1": filename_a,
+    "fichier_2": filename_b,
+    "fichier_3": filename_c,
+    "fichier_4": filename_d,
+    "fichier_5": filename_e,
+    "fichier_6": filename_f,
+    "fichier_7": filename_g
+}
+
+for key, filename in file.items():
+    path_complet = os.path.join(dossier, filename)
+    file_instance = Files(path_complet)
+    file_instance.exporte_csv(f"Resultat_csv/{filename}.csv")
+    reader = PdfReader(file_instance.filename)
+    page = reader.pages[0]  # Première page, remplacez 0 par le numéro de la page souhaitée
+    page.extract_text(visitor_text=file_instance.visitor_body)
+    text_body = "".join(file_instance.parts)
+
     
-    #Trrouver des critere qui dit a chaque fois je suis dans le header faire ca et quand je suis dans le footer fait ca 
-    #delimiter le header et le footer 
-    
-    
-    #simple trouver un moyen de definir y et le passer en parametre 
-    
-            
-    def without_some_word(self):
-        not_this_world= ["S.A. au capital de 643 054 425 euros"]
-        
-            
-
-
-
-
-
-file = Files("etude_tarifaire_sante_logo_(1).pdf")
-
-reader = PdfReader(file.filename)
-print("-----------------------------")
-page = reader.pages[0]  # Première page, remplacez 0 par le numéro de la page souhaitée
-
-page.extract_text(visitor_text=file.visitor_body)
-text_body = "".join(file.parts)
-
-print(text_body)
-
-
-openAi=ZeroShotGPTClassifier()
-openAi.fit(None, ['Honoraires', 'Forfait Journalier Hospitalier', 'Etablissements conventionnés',"Etablissements non conventionnés","Divers","Forfait Naissance","Honoraires Médicaux","Analyses et examens de laboratoires","Honoraires paramédicaux","Médicaments","Matériel Médical","Divers","Equipements 100% Santé *","Equipements de classe II ***","Optique,Equipements 100% Santé *","Equipements de classe B ou mixtes **","Autres","Dentaire","Soins et Prothèses 100% Santé *",
-"Soins","Prothèses","Traitement d'orthodontie","Autres","Médecine douce / Prévention","Fonds Social/ Spécificités CCN","Services"])
-x= file.parts
-labels = openAi.predict(x)
-
-for i in range(len(labels)):
-    print(labels[i],x[i],"\n")
-
-
-        
-
