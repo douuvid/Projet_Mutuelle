76e02d19543a0efc4e454cd33bd03c0b0e5faa31
Douuvid <silasiharis@gmail.com>
2023-06-19 15:59:25 +0200

    Ca fonctionne bien j'ai re installer le necessaire

--- test_by_word.py
+++ test_by_word.py
@@ -1,9 +1,27 @@
 import os
 import ocrmypdf
 import pandas as pd
+
+
 import fitz
+
 import tabula
 import re
+import spacy
+
+"""_Bibliotheque _
+Os permet==> d'interagir avec le système d'exploitation,
+ocrmypdf est utilisé pour effectuer l'OCR (reconnaissance optique de caractères) sur les fichiers PDF,
+pandas est une bibliothèque pour la manipulation de données, 
+fitz est utilisé pour extraire le texte d'un fichier PDF, 
+tabula est utilisé pour détecter et extraire les tableaux d'un fichier PDF
+re est utilisé pour effectuer des opérations de recherche et de correspondance de motifs (regex).
+
+
+   
+"""
+
+ 
 
 # Definition of the list of excluded words
 black_list = ["de", "la", "par", "et", "le", "des",
@@ -14,7 +32,51 @@ black_list = ["de", "la", "par", "et", "le", "des",
               "recueillons", "mais", "cachet", "cedex",
               "janvier", "une", "www", "que"]
 
+stopwords = [
+    "de", "la", "par", "et", "le", "des", "à", "les", "au", "en", "du", "ou",
+    "est", "l", "dans", "pour", "dont", "soit", "allianz", "d", "powered", "by",
+    "org", "tel", "marketing", "toutes", "utilisons", "recueillons", "mais",
+    "cachet", "cedex", "janvier", "une", "www", "que", "sont", "sur", "avec",
+    "il", "ne", "ce", "nous", "vous", "ils", "elles", "je", "j", "tu", "cette",
+    "cet", "ces", "sa", "son", "ses", "notre", "nos", "votre", "vos", "leur",
+    "leurs", "mon", "ma", "mes", "ton", "ta", "tes", "se", "qui", "quoi", "où",
+    "quand", "comment", "pourquoi", "quel", "quelle", "quels", "quelles",
+    "être", "avoir", "faire", "aller", "dire", "pouvoir", "vouloir", "venir",
+    "voir", "savoir", "falloir", "devoir", "prendre", "donner", "trouver",
+    "aimer", "dire", "partir", "venir", "temps", "jour", "année", "fois",
+    "après", "avant", "pendant", "maintenant", "plus", "moins", "très", "peu",
+    "trop", "aussi", "bien", "mal", "vraiment", "enfin", "alors", "ensuite",
+    "donc", "mais", "car", "parce", "que", "si", "ou", "non", "avec", "sans",
+    "chez", "vers", "pour", "jusque", "depuis", "avant", "après", "entre",
+    "sous", "sur", "dans", "hors", "devant", "derrière", "autour", "tout",
+    "tous", "toute", "toutes", "rien", "aucun", "chaque", "plusieurs", "autre",
+    "autres", "quelque", "quelques", "beaucoup", "certains", "certaines",
+    "ainsi", "alors", "alors", "lorsque", "alors", "autrement", "aussitôt",
+    "auparavant", "aussi", "beaucoup", "bientôt", "cependant", "certes",
+    "chaque", "d'abord", "davantage", "déjà", "demain", "dernièrement",
+    "dès", "désormais", "devant", "dorénavant", "doucement", "d'un", "durant",
+    "en", "encore", "ensemble", "ensuite", "entre", "environ", "et", "exactement",
+    "fort", "franchement", "froidement", "généralement", "grâce", "hélas",
+    "hier", "honnêtement", "immédiatement", "jamais", "jusqu'à", "là", "lentement",
+    "maintenant", "mal", "malheureusement", "moins", "néanmoins", "nettement",
+    "non", "nonchalamment", "nonobstant", "normalement", "notamment", "oui",
+    "par", "parfois", "partout", "pas", "pauvrement", "peu", "peut-être",
+    "plutôt", "précisément", "premièrement", "presque", "pourtant", "quand",
+    "quant", "quasiment", "que", "quelquefois", "quoique", "rapidement",
+    "rarement", "réellement", "régulièrement", "s'il", "sincèrement", "soi",
+    "souvent", "strictement", "subitement", "surtout", "tantôt", "tard",
+    "tellement", "tôt", "toujours", "tous", "toutefois", "très", "vite",
+    "vivement", "volontiers", "vraiment", "maintenant"
+]
+
+
 # Definition of the function test_by_word
+
+
+"""test_by_word()
+ ==> prend un texte en entrée et renvoie une liste de mots avec leur pourcentage d'occurrence dans le texte.
+ Elle utilise la liste d'exclusion des mots (black_list) pour exclure certains mots courants de l'analyse.
+"""
 def test_by_word(text, number):
     try:
         text_split = re.findall(r"[\w']+", text)
@@ -40,11 +102,94 @@ def test_by_word(text, number):
         return my_list
 
     except Exception as e:
-        print("Une exception s'est produite lors de l'exécution de test_by_word:", e)
+        print("Aucun mot", e)
         return None
 
-# Definition of the function determine_document_type
+
+
+
+
+
+
+
+
+"""determine_document_type()
+
+prend une liste de mots avec leur pourcentage d'occurrence et détermine le type de document en fonction des mots clés et des scores associés.
+Elle utilise les dictionnaires mots_cles_contrat_sante, mots_cles_contrat_iard et mots_cles_contrat_assurance_vie pour définir les mots clés 
+et les scores associés à chaque type de contrat.
+
+Elle calcule ensuite des scores pour chaque type de contrat en fonction de la correspondance des mots clés dans la liste de mots
+et renvoie le type de document le plus probable.
+"""
+
+
 def determine_document_type(word_list):
+    mots_cles_contrat_sante = {
+        "soins": {
+            "hospitalisation": 0.9,
+            "consultation": 0.7,
+            "médecin": 0.8,
+            "spécialiste": 0.8,
+            "chirurgie": 0.7,
+            "médicaments": 0.6,
+            "frais médicaux": 0.7,
+            "remboursement" : 0.7,
+            "assurance maladie" : 0.8,
+            "garantie" : 0.6,
+            "couverture" : 0.7,
+            "mutuelle" : 0.6,
+            "pharmacie" : 0.6,
+            "hospitalier" : 0.7,
+            "maternité" : 0.6,
+            "dentaire" : 0.6,
+            "optique" : 0.6
+        },
+        # Autres mots clés pour le contrat santé...
+    }
+
+    mots_cles_contrat_iard = {
+        "habitation": {
+            "maison": 0.8,
+            "appartement": 0.7,
+            "logement": 0.6,
+            "dommages": 0.7,
+            "assurance habitation": 0.9,
+            "biens immobiliers": 0.7,
+            "propriété": 0.6,
+            "responsabilité civile" : 0.8,
+            "sinistre" : 0.7,
+            "vol" : 0.7,
+            "dégât des eaux" : 0.7,
+            "catastrophes naturelles" : 0.7,
+            "protection juridique" : 0.6,
+            "assurance automobile" : 0.8,
+            "assurance moto" : 0.7,
+            "assurance voyage" : 0.6,
+            "assurance responsabilité professionnelle" : 0.7
+        },
+        # Autres mots clés pour le contrat IARD...
+    }
+
+    mots_cles_contrat_assurance_vie = {
+        "capital décès": {
+            "bénéficiaire": 0.8,
+            "succession": 0.7,
+            "capital décès": 0.9,
+            "épargne": 0.7,
+            "rente" : 0.7,
+            "succession" : 0.8,
+            "investissement" : 0.6,
+            "bénéficiaire" : 0.7,
+            "rendement" : 0.6,
+            "clause bénéficiaire" : 0.8,
+            "capitalisation" : 0.6,
+            "transmission" : 0.7,
+            "fiscalité": 0.6
+        },
+        # Autres mots clés pour le contrat d'assurance vie...
+    }
+
     contrat_sante_score = 0
     contrat_iard_score = 0
     contrat_assurance_vie_score = 0
@@ -64,124 +209,40 @@ def determine_document_type(word_list):
             for mot_cle, score in mots_cles.items():
                 if word.lower() == mot_cle.lower():
                     contrat_assurance_vie_score += percentage * score
+                    
+        
+        
+        
 
-    if contrat_sante_score > contrat_iard_score:
+    print(f"Score contrat santé: {contrat_sante_score}")
+    print(f"Score contrat IARD: {contrat_iard_score}")
+    print(f"Score contrat assurance vie: {contrat_assurance_vie_score}")
+
+    if contrat_sante_score > contrat_iard_score and contrat_sante_score > contrat_assurance_vie_score:
         return "Contrat Santé"
-    elif contrat_iard_score > contrat_sante_score:
+    elif contrat_iard_score > contrat_sante_score and contrat_iard_score > contrat_assurance_vie_score:
         return "Contrat IARD"
-    elif contrat_assurance_vie_score > contrat_iard_score and contrat_assurance_vie_score > contrat_sante_score:
+    elif contrat_assurance_vie_score > contrat_sante_score and contrat_assurance_vie_score > contrat_iard_score:
         return "Contrat Assurance Vie"
     else:
-        return "Type de document indéterminé"
+        return "Type de contrat indéterminé"
+    
 
-# Definition of keywords for health, IARD, and life insurance contracts
-mots_cles_contrat_sante = {
-    "soins": {
-        "hospitalisation": 0.9,
-        "consultation": 0.7,
-        "médecin": 0.8,
-        "spécialiste": 0.8,
-        "chirurgie": 0.7,
-        "médicaments": 0.6,
-        "frais médicaux": 0.7
-    },
-    "prothèses": {
-        "dentaires": 0.8,
-        "auditives": 0.7,
-        "orthopédiques": 0.6,
-        "prothèses": 0.7,
-        "remboursement": 0.6,
-        "appareillage": 0.5
-    },
-    "dépenses de santé": {
-        "remboursement": 0.7,
-        "prestations": 0.6,
-        "frais médicaux": 0.7,
-        "dépenses de santé": 0.8,
-        "couverture santé": 0.6
-    },
-    "optique": {
-        "lunettes": 0.7,
-        "verres": 0.6,
-        "monture": 0.5,
-        "ophtalmologie": 0.7,
-        "remboursement": 0.6,
-        "optique": 0.8
-    },
-    "maternité": {
-        "grossesse": 0.8,
-        "accouchement": 0.9,
-        "prénatal": 0.7,
-        "postnatal": 0.7,
-        "maternité": 0.9
-    },
-    "dentaire": {
-        "dentaire": 0.8,
-        "soins dentaires": 0.7,
-        "orthodontie": 0.6,
-        "prothèses dentaires": 0.7,
-        "dents": 0.7
-    }
-}
 
-mots_cles_contrat_iard = {
-    "habitation": {
-        "maison": 0.8,
-        "appartement": 0.7,
-        "logement": 0.6,
-        "dommages": 0.7,
-        "assurance habitation": 0.9,
-        "biens immobiliers": 0.7,
-        "propriété": 0.6
-    },
-    "automobile": {
-        "voiture": 0.9,
-        "véhicule": 0.8,
-        "collision": 0.7,
-        "accident": 0.8,
-        "assurance auto": 0.9,
-        "véhicule motorisé": 0.8,
-        "conduite": 0.7,
-        "sinistre": 0.6
-    },
-    "responsabilité civile": {
-        "indemnisation": 0.7,
-        "préjudice": 0.8,
-        "responsabilité": 0.7,
-        "dommage": 0.6,
-        "indemnité": 0.7,
-        "dommage matériel": 0.8,
-        "dommage corporel": 0.9
-    }
-}
+"""le directory fdp
 
-mots_cles_contrat_assurance_vie = {
-    "capital décès": {
-        "bénéficiaire": 0.8,
-        "succession": 0.7,
-        "capital décès": 0.9
-    },
-    "épargne": {
-        "fonds en euros": 0.7,
-        "unités de compte": 0.6,
-        "primes": 0.7,
-        "rachat partiel": 0.8
-    },
-    "avance sur contrat": {
-        "avance sur contrat": 0.9,
-        "clause bénéficiaire": 0.8,
-        "valeur de rachat": 0.7
-    }
-}
-
+   définissent le répertoire contenant les fichiers PDF à traiter et 
+   créent une liste de fichiers PDF se terminant par l'extension ".pdf" dans ce répertoire.
+    """
 # Chemin du répertoire contenant les fichiers PDF
 directory = "./Contrat/"
-
 # Liste des fichiers PDF à traiter
 file_list = [f for f in os.listdir(directory) if f.endswith('.pdf')]
 
 
+
 def ocr_pdf(file_path):
+    
     # OCR du fichier PDF
     ocr_file_name = f'OCR_{os.path.basename(file_path)}'
     ocr_directory = os.path.join(directory, "./Contrat/OCR_Resultat")
@@ -212,6 +273,8 @@ def extract_text_and_tables(ocr_file_path, file_path):
 
     return extracted_text
 
+
+
 ##Analyse des mots, détermination du type de document et affichage du contenu OCR
 def process_document(file_name):
     try:
@@ -238,6 +301,7 @@ def process_document(file_name):
 
         for encoding in encodings:
             try:
+                
                 with open(ocr_file_path, 'rb') as f:
                     content = f.read().decode(encoding)
                 print(f"Contenu du document OCR ({encoding}): {file_name}")
@@ -253,3 +317,26 @@ def process_document(file_name):
         print(f"Fichier introuvable : {file_name} - {e}")
     except Exception as e:
         print(f"Une exception s'est produite lors du traitement du fichier : {file_name} - {e}")
+
+
+
+nlp = spacy.load('fr_core_news_sm')
+stopwords = spacy.lang.fr.stop_words.STOP_WORDS
+
+
+def remove_stopwords(text):
+    doc = nlp(text)
+    tokens = [token.text for token in doc if token.text.lower() not in stopwords]
+    return " ".join(tokens)
+
+
+
+
+
+file_names = ['etude_tarifaire_sante_logo_(1).pdf']
+
+for file_name in file_names:
+    process_document(file_name)
+    
+
+
